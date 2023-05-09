419ccd5d62c7bfd6f3106df15f54f521ed6c8cfe
Jonas <jonas.gantar@gmail.com>
2023-05-09 07:24:15 -0700

    routes

---
+++ src/client/components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div>
+      2MainTheTrackerTheTrackerTheTrackerTheTrackerTheTrackerTheTracker
+    </div>
+  );
+}
+
+export default Main;
