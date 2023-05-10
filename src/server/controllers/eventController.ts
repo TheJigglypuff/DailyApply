a7ac463590d837b313c4d799e0071a134252cd1b
Bryent <bryents@gmail.com>
2023-05-10 10:20:57 -0400

    createNewEvent, getYourEvents, getAllUsers

--- src/server/controllers/eventController.ts
+++ src/server/controllers/eventController.ts
@@ -4,7 +4,6 @@ import { Express, Request, Response, NextFunction } from 'express';
 
 module.exports = {
     createNewEvent: async (req: Request, res: Response, next: NextFunction) => {
-        //need to eventually change user_id to be retrieved from a jwt created on login/signup
         const { email, company, role, dateApplied } = req.body;
         try {
             //get user_id
@@ -16,18 +15,6 @@ module.exports = {
             //update user's total events
             const newTotalEvents = await query(`UPDATE users SET total_events = total_events + 1 WHERE user_id = $1;`, [user_id]);
 
-            
-            // const result = await query(`INSERT INTO users (email, name) VALUES ($1, $2) returning name`, [email, name]);
-            // const user_id = await query(`UPDATE users SET users.total_events = users.total_events WHERE users.user_id = $1`, [user_id])
-            // CREATE TABLE events (
-            //     event_id SERIAL PRIMARY KEY,
-            //     company varchar(255) NOT NULL,
-            //     role varchar(255) NOT NULL,
-            //     dateApplied DATE,
-            //     user_id int,
-            //     FOREIGN KEY (user_id) REFERENCES users(user_id)
-        
-
         } catch (err) {
             console.log('error', err);
             next(err);
@@ -35,8 +22,16 @@ module.exports = {
         
     },
 
-    getYourEvents: () => {
-
+    getYourEvents: async (req: Request, res: Response, next: NextFunction) => {
+        const user_id = 1;
+        try {
+            const myEvents = await query(`SELECT * FROM events WHERE user_id = $1`, [user_id]);
+            res.locals.myEvents = myEvents.rows;
+            return next();
+        } catch (err) {
+            console.log('error', err);
+            next(err);
+        }
     },
 
 }
